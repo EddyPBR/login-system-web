d7d4462d2e3cd168eaa584df51303a8a0e8336a2
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-29 10:20:11 -0300

    :package: Setting responvivity for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,17 +5,25 @@ import { Link } from "react-router-dom";
 import authenticationSvg from "../../assets/images/authentication.svg";
 
 // STYLES
-const Container = styled.div`
+const Container = styled.main`
   min-height: 100vh;
   width: 90vw;
   max-width: 1170px;
 
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-gap: 120px;
+  column-gap: 120px;
 
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 960px) {
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
 `;
 
 const Column = styled.div`
@@ -24,6 +32,14 @@ const Column = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 960px) {
+    margin: 40px 0px;
+  }
+
+  @media (max-width: 380px) {
+    margin: 20px 0px;
+  }
 `;
 
 const Title = styled.h1`
@@ -31,6 +47,11 @@ const Title = styled.h1`
   font-weight: 400;
   line-height: 9.2rem;
   margin: 0;
+
+  @media (max-width: 580px) {
+    font-size: 4.2rem;
+    line-height: 7.2rem;
+  }
 `;
 
 const Text = styled.p`
@@ -39,19 +60,23 @@ const Text = styled.p`
   margin-top: 30px;
 `;
 
-const Button = styled.a`
+const Button = styled(Link)`
   width: 100vw;
   height: 100vh;
   max-width: 150px;
   max-height: 50px;
 
+  margin-top: 60px;
+  text-decoration: none;
+
   font-size: 1.8rem;
   font-weight: bold;
-  color: #fff;
+  color: var(--color-text);
 
   background: linear-gradient(
     180deg,
     var(--green-light) 0%,
+    var(--green-dark) 60%,
     var(--green-dark) 100%
   );
   box-shadow: 2px 2px 4px 4px rgba(0, 0, 0, 0.25);
@@ -60,35 +85,27 @@ const Button = styled.a`
   user-select: none;
   transition: ease-in-out 0.2s;
 
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   justify-self: flex-start;
   align-self: flex-start;
 
   &&:hover {
+    color: #fff;
     cursor: pointer;
-    filter: brightness(0.9);
+    filter: brightness(0.8);
     box-shadow: 3px 3px 5px 5px rgba(0, 0, 0, 0.25);
   }
 
-  && > a {
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    text-decoration: none;
-    color: var(--color-title);
-  }
-
-  &&:hover > a {
-    filter: brightness(3);
+  @media (max-height: 640px) {
+    margin-top: 40px;
   }
 `;
 
 const Svg = styled.img`
   width: 100%;
+  max-width: 640px;
 `;
 
 const Home: React.FC = () => {
@@ -100,9 +117,7 @@ const Home: React.FC = () => {
           A simple login system just for studies and code reuse in projects.
           Feel free and I hope you like it.
         </Text>
-        <Button>
-          <Link to="/login">Acessar</Link>
-        </Button>
+        <Button to="/login">Acessar</Button>
       </Column>
       <Column>
         <Svg src={authenticationSvg} alt="A login system api" />
