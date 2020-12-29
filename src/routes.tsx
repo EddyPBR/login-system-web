e18c4508e67801c94df31691f898bb21887f8d43
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-29 13:06:56 -0300

    :package: Adding login page route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/login" exact component={Login} />
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
