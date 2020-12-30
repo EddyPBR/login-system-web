5093d37183572301fab8bf595dd67a95909bbe5f
EddyPBR <edvaldojunior1310@gmail.com>
2020-12-30 16:11:03 -0300

    :package: Adding route for recover page

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Redirect, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Recover from "./pages/Recover";
 
 const Routes: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const Routes: React.FC = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Login} />
+        <Route path="/recover" exact component={Recover} />
         <Redirect to="/" />
       </Switch>
     </BrowserRouter>
